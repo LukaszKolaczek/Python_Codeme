b39d49a87446ac7899d9c1b97c40d7ae0fb58502
LukaszKolaczek <lkolaczek@wp.pl>
2022-03-09 16:17:14 +0100

    FOR

---
+++ 03-IF/FOR-01.py
@@ -0,0 +1,9 @@
+# twórz listę przedmiotów, które zabierzesz na samotną wyprawę w góry.
+# Wyświetl nazwę właśnie spakowanego przedmiotu,
+# po ostatnim przedmiocie pokaż informację: “Great, we are ready!”
+
+items = ['buty terkingowe', 'czapkę', 'okulary przeciwsłoneczne']
+for i in items:
+    print(i)
+
+print('Great, we are ready!')
